aadbe7ef110fc5ebc42ceb68f5f94f7de3782142
hassuni <hasss95@hotmail.com>
2020-12-18 01:08:07 +0100

    added dropwdown and Submit button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
+
+import Dropdown from "react-dropdown";
+
+
+
+class App extends Component {
+
+
+
+state = {
+  selectedOption: "None",
+  dropDownOptions: ["Hotels", "Flights", "Tourist"]
+}
+
+
+  _onSelect = (selectedOption) => {
+    this.setState({ selectedOption });
+   
+  };
+
+
+  render() {
+    return(
     <div className="App">
 
+  <Dropdown
+   value={this.state.selectedOption}
+   onChange={this._onSelect}
+   options={this.state.dropDownOptions}
+ />
+
+   <button
+                        onClick={this.handleSubmit}
+                        className="button is-success"
+                      >
+                        Hent Data
+                      </button>
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
